3267658e23ee74946b2d41c4c16c10852830c4e6
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-07-31 11:36:16 -0400

    package restructured; name changed

---
+++ mentor/__init__.py
@@ -0,0 +1,3 @@
+from .db_reader import construct_marker_mat_from_db, to_yaml
+
+__all__ = ["construct_marker_mat_from_db", "to_yaml"]
--- setup.py
+++ setup.py
@@ -4,28 +4,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="astir-marker-generator",
+    name="mentor",
     version="0.0.1",
     author="Jinyu Hou",
     author_email="jhou@lunenfeld.ca",
     description=" ",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/camlab-bioml/astir-marker-generator",
-    packages=["astir-marker-generator"],
+    url="https://github.com/camlab-bioml/mentor",
+    packages=["mentor"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
-    license="GPLv3",
+    license="GPLv2",
     install_requires=[
         "pandas",
         "pyyaml",
-        "argparse",
-        "os"
+        "argparse"
     ],
     include_package_data=True,
-    scripts=["bin/astir-marker"],
+    scripts=["bin/mentor"],
 )
