9b4d4bd2e58a089abcf913532fafc75cf9734b02
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-07-31 11:38:12 -0400

    removed redundant files

--- astir_marker_generator/__init__.py
+++
@@ -1,3 +0,0 @@
-from .marker_generator import construct_marker_mat_from_db, to_yaml
-
-__all__ = ["construct_marker_mat_from_db", "to_yaml"]
--- astir_marker_generator/marker_generator/__init__.py
+++
@@ -1,1 +0,0 @@
-from .db_reader import construct_marker_mat_from_db, to_yaml
